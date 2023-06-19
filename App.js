1e6bce1e55918ff5e544f5ffd2e4abf71e909d10
manthand23 <66882196+manthand23@users.noreply.github.com>
2023-06-19 17:03:26 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+import HomeScreen from './Screens/Home';
+import CalculatorsScreen from './Screens/Calculators';
+import ContributorsScreen from './Screens/Contributors';
+import ToC from './Screens/ToC';
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={ HomeScreen } />
+        <Drawer.Screen name="Table of Contents" component={ ToC } />
+        <Drawer.Screen name="Calculators" component={ CalculatorsScreen } />
+        <Drawer.Screen name="Contributors" component={ ContributorsScreen } />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
