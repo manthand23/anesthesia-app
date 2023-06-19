0e45d0cb074dadd7bc6e642aba257063757ed0d7
Dhyey Bhavsar <dhyeybhavsar2000@gmail.com>
2023-06-19 19:30:31 -0400

    Updated preOP page

--- Content/preOp/PreOpAssessment.js
+++ Content/preOp/PreOpAssessment.js
@@ -2,14 +2,14 @@ import 'react-native-gesture-handler';
 import React, { useEffect } from 'react';
 import { ScrollView, View, Text, StyleSheet, Dimensions } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { SelectList } from 'react-native-dropdown-select-list';
-import goalsPreOp from './goalsPreOp';
-import {Iframe} from 'react-iframe'
+//import { SelectList } from 'react-native-dropdown-select-list';
+import { data } from './1';
+import Components from "../../components";
 
 const PreOpAssessment = () => {
     return (
         <SafeAreaView edges={['bottom']}>
- 
+            {data.body.map(block => Components(block))}
         </SafeAreaView>
     );
 }
