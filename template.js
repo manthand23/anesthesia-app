75960aba9c48c48f316caf492536ea7ade9f1c2f
Dhyey Bhavsar <dhyeybhavsar2000@gmail.com>
2023-09-21 20:40:12 -0400

    Added content template

---
+++ template.js
@@ -0,0 +1,18 @@
+export const data = {
+    body: [
+        { component: "title", text: "" },
+        { component: "line" },
+        { component: "heading", text: "" },
+        { component: "bullets", text: [""] },
+        {
+            component: "table", tableHead: ["", ""], tableData: [
+                ["", ""],
+                ["", ""]
+            ]
+        },
+        { component: "photo", src: 1 },
+        { component: "bullets", text: [""] },
+        { component: "photo", src: 2 },
+        { component: "quiz", question: "", options: [""], answer: "" },
+    ]
+};
