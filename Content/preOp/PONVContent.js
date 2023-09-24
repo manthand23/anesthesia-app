50db0a03f69b1e5df099697125bb8b9ec38e1a49
kair25 <106719643+kair25@users.noreply.github.com>
2023-09-24 11:29:58 -0400

    Update PONVContent.js

--- Content/preOp/PONVContent.js
+++ Content/preOp/PONVContent.js
@@ -2,12 +2,10 @@ export const data = {
     body: [
         { component: "title", text: "Post-operative Nausea/Vomiting" },
         { component: "line" },
-        { component: "heading", text: "Chemoreceptor trigger zone (CTZ)" },
-        { component: "bullets", text: ["Area of the brainstem which is stimulated through opioid, serotonin (5HT3), histamine, dopamine, and muscarinic ACh receptors", "Adequate stimulation will communicate signaling to initiate vomiting."] },
-        { component: "line" },
+        { component: "para", bold: "Chemoreceptor trigger zone (CTZ):  ", text: "Area of the brainstem which is stimulated through opioid, serotonin (5HT3), histamine, dopamine, and muscarinic ACh receptors. Adequate stimulation will communicate signaling to initiate vomiting"},
         {
             component: "table", tableHead: ["Simplified Apfel score for PONV", "Other risk factors:"], tableData: [
-                ["Female \n- Hx of PONV or motion sickness \n- Non-smoker \n- Post-operative opioids", "Younger age \n- Use of volatile anesthetics \n- Surgery >2h \n- Pregnancy \n- Abdomen, genital, ENT/opthalmic, neurosurgery"]
+                ["- Female \n- Hx of PONV or motion sickness \n- Non-smoker \n- Post-operative opioids", "- Younger age \n- Use of volatile anesthetics \n- Surgery >2h \n- Pregnancy \n- Abdomen, genital, ENT/opthalmic, neurosurgery"]
             ]
         },
         { component: "line" },
